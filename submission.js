39215b20e6472c8e2c2a8d7551db59c1fdc0297f
Audrey Clark <37598784+audreyclarkdev@users.noreply.github.com>
2023-12-30 23:50:47 -0600

    Update submission.js
    Saving the first three functions

--- submission.js
+++ submission.js
@@ -1,13 +1,43 @@
 const findSum = function(array) {
-  // your code here - don't forget to return a number!
+  let sum = 0;
+  for (let i = 0; i < array.length; i++){
+    sum += array[i];
+  };
+  return sum;
 };
 
 const findFrequency = function(array) {
-  // your code here - don't forget to return an object!
+  const obj = {};
+  let mostFreq = array[0]
+  let maxCount = 1;
+  
+  for (let i = 0; i < array.length; i++){
+    let letter = array[i];
+    if (obj[letter]) {
+      obj[letter] ++;
+    } else {
+      obj[letter] = 1;
+    };
+    if (obj[letter] > maxCount) {
+      mostFreq = letter;
+      maxCount = obj[letter];
+    };
+    if (obj[letter] < maxCount) {
+      leastFreq = letter;
+    };
+  };
+  return "most: " + mostFreq + ", "+ "least: " + leastFreq;
 };
 
 const isPalindrome = function(str) {
-  // your code here - don't forget to return a boolean!
+  const input = str.toLowerCase();
+  const strLen = input.length;
+  for (let i = 0; i < strLen / 2; i++) {
+    if (input[i] !== input[strLen - 1 - i]) {
+      return false;
+    };
+  };
+  return true;
 };
 
 const largestPair = function(array) {
