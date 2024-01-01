47f25e0657300ab8ffae09491720bb4544c91ea8
Audrey Clark <37598784+audreyclarkdev@users.noreply.github.com>
2023-12-31 22:58:48 -0600

    Update submission.js
    Edited largestPair() problem

--- submission.js
+++ submission.js
@@ -42,8 +42,8 @@ const isPalindrome = function(str) {
 
 const largestPair = function(array) {
   let largest = array[0] * array[1];
-  for(let i = 1; i < array.length - 1; i++){
-    let product = array[i] * array[i - 1];
+  for(let i = 0; i < array.length; i++){
+    let product = array[i] * array[i + 1];
     if (product > largest){
       largest = product;
     }
